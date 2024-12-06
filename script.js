ff554c074dcfc5d86aa934fd4dd0b0607d3f920b
Aaditya Vinayak <vaaditya320@gmail.com>
2024-12-06 23:02:37 +0530

    added copy link functionality

--- script.js
+++ script.js
@@ -8,3 +8,12 @@ cards.forEach(card => {
         card.classList.toggle('is-flipped');
     });
 });
+
+document.getElementById('copyEsp8266').addEventListener('click', function() {
+    const link = 'http://arduino.esp8266.com/stable/package_esp8266com_index.json'; // Replace with actual link
+    navigator.clipboard.writeText(link)
+})
+document.getElementById('copyEsp32').addEventListener('click', function() {
+    const link = 'https://dl.espressif.com/dl/package_esp32_index.json'; // Replace with actual link
+    navigator.clipboard.writeText(link)
+})
